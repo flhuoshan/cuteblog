2d31a2a57760f3b55981e5ff8b3c6d2ac47470c4
flhuoshan <475296958@qq.com>
2018-01-14 21:44:36 +0800

    [add]欢迎页面，博客主页等

--- cuteblog/settings.py
+++ cuteblog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cuteblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mainblog/templates'), os.path.join(BASE_DIR, 'welcome/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- welcome/views.py
+++ welcome/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def hello(request):
-    return HttpResponse("Hello World...")
+def welcome(request):
+    return render(request, "welcome/welcome.html")
